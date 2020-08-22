7e0d243940913a9769d07e433d2410f10a06f7a6
Sambhu Singh <sambhu.raj.singh@gmail.com>
2020-08-22 10:37:29 +0100

    [FEATURE] - Add Electic Oven Class

---
+++ app/Utilities/ElectricOven.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Utilities;
+
+class ElectricOven implements Oven
+{
+    /**
+     * @inheritDoc
+     */
+    public function heatUp(): Oven
+    {
+        // TODO: Implement heatUp() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function bake(Pizza &$pizza): Oven
+    {
+        // TODO: Implement bake() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function turnOff(): Oven
+    {
+        // TODO: Implement turnOff() method.
+    }
+}
