7a557838d95b2611efaf4a65651c903f9e792e08
Sambhu Singh <sambhu.raj.singh@gmail.com>
2020-08-22 11:02:25 +0100

    [TEST] - change to this instead

--- tests/Feature/OrderingTest.php
+++ tests/Feature/OrderingTest.php
@@ -19,7 +19,7 @@ class OrderingTest extends TestCase
     public function __construct($name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
-        parent::setUp();
+        $this->setUp();
         $this->luigis = new Luigis();
     }
 
