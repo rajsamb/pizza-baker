d40b34ff00d43141db6e81729039353ca04eb2d0
Sambhu Singh <sambhu.raj.singh@gmail.com>
2020-08-23 11:49:51 +0100

    [FEATURE] - Add the functionality to add the ingredient on the recipe

--- app/Services/CustomRecipeBuilder.php
+++ app/Services/CustomRecipeBuilder.php
@@ -2,17 +2,46 @@
 
 namespace App\Services;
 
+use App\Models\Ingredient;
 use App\Models\Recipe;
+use App\Models\RecipeIngredient;
 
 class CustomRecipeBuilder
 {
     private const BASE_PIZZA_PRICE = 5.00;
 
-    public function build(string $customPizzaName): Recipe
+    /**
+     * @param string $customPizzaName
+     * @param array $ingredients
+     * @return Recipe
+     */
+    public function build(string $customPizzaName, array $ingredients): Recipe
     {
-        return Recipe::updateOrCreate([
+        $receipe = Recipe::updateOrCreate([
             'name' => $customPizzaName,
             'price' => self::BASE_PIZZA_PRICE
         ]);
+
+        if (count($ingredients) > 0) {
+            foreach ($ingredients as $ingredientToAdd) {
+                $ingredient = Ingredient::whereName($ingredientToAdd['name'])->first();
+
+                $lastRowId = RecipeIngredient::latest('id')->first()->id;
+
+                RecipeIngredient::updateOrCreate(
+                    [
+                        'id' => $lastRowId + 1,
+                        'recipe_id' => $receipe->id,
+                        'ingredient_id' => $ingredient->id,
+                        'amount' => $ingredientToAdd['qty']
+                    ]
+                );
+
+                $receipe->price += $ingredient->price * $ingredientToAdd['qty'];
+                $receipe->save();
+            }
+        }
+
+        return $receipe;
     }
 }
